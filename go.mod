70a2ef676df49457cf584c9154188f99a19ccbd1
babanini95 <arobbanii5@gmail.com>
2025-05-22 09:33:50 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/babanini95/pokedexcli
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	output := make([]string, 0)
+
+	return output
+}
