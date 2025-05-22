50087f1eb14f1fd66276eec793b03a66e5174d5b
babanini95 <arobbanii5@gmail.com>
2025-05-22 17:53:10 +0800

    Refactored util and command logic from the main script

---
+++ command.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func generateCommand() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	output := "Welcome to the Pokedex!\nUsage:\n"
+
+	for _, command := range commands {
+		output += fmt.Sprintf("\n%s: %s", command.name, command.description)
+	}
+
+	fmt.Println(output)
+	return nil
+}
--- main.go
+++ main.go
@@ -4,16 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
-
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-var commands map[string]cliCommand
 
 func main() {
 	commands = generateCommand()
@@ -39,41 +30,4 @@ func main() {
 			}
 		}
 	}
-}
-
-func cleanInput(text string) []string {
-	cleanText := strings.Fields(strings.ToLower(text))
-	return cleanText
-}
-
-func generateCommand() map[string]cliCommand {
-	return map[string]cliCommand{
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-	}
-}
-
-func commandExit() error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	output := "Welcome to the Pokedex!\nUsage:\n"
-
-	for _, command := range commands {
-		output += fmt.Sprintf("\n%s: %s", command.name, command.description)
-	}
-
-	fmt.Println(output)
-	return nil
 }
